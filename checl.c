f9539786155b99e20a63fc57d941c94e7146837d
Chelsea Wright <143695183+syzygy-0@users.noreply.github.com>
2023-10-04 21:19:16 +0000

    checkmark pattern

---
+++ checl.c
@@ -0,0 +1,14 @@
+/*Creates a checkmark pattern*/
+#include <stdio.h>
+
+int main(void)
+{
+        printf("       *\n");
+        printf("      *\n");
+        printf("     *\n");
+        printf("*   *\n");
+        printf(" * *\n");
+        printf("  *\n");
+
+        return 0;
+}
